24f43592854f52772076a5a32da5b1c1cd0c0a4e
MarijaVrljanac <marija.vrljanac@gmail.com>
2020-12-20 23:17:01 +0100

    First commit

---
+++ js/katalog.js
@@ -0,0 +1,14 @@
+
+  function funkcija() {
+      var proizvod;
+      var i;
+      proizvod = document.getElementsByClassName("proizv");
+     
+  for (i = 0; i < proizvod.length; i++) {
+    console.log(proizvod[i].innerHTML);
+  }
+    }
+
+
+
+
